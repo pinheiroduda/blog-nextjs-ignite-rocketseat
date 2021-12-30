546e48a5bd98c5bff7a6a2a5a0ebaa6b61292345
Eduarda <eduardapinheiro1302@gmail.com>
2021-12-30 15:40:48 -0300

    feat: render home header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { GetStaticProps } from 'next';
+import Header from '../components/Header';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -24,9 +25,11 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return (
+    <Header />
+  )
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
