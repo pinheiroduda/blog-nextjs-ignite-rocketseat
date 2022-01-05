008b46e88c17244483ed597c6a3820e6143f5de5
Eduarda <eduardapinheiro1302@gmail.com>
2022-01-05 14:58:03 -0300

    feat: configures getStaticPaths to generate post pages

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,8 +1,12 @@
-import { format } from 'date-fns';
-import ptBR from 'date-fns/locale/pt-BR';
 import { GetStaticPaths, GetStaticProps } from 'next';
+import { useRouter } from 'next/router';
+
 import Header from '../../components/Header';
 
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+
+import Prismic from '@prismicio/client'
 import { getPrismicClient } from '../../services/prismic';
 
 import commonStyles from '../../styles/common.module.scss';
@@ -30,21 +34,41 @@ interface PostProps {
 }
 
 export default function Post({ post }: PostProps) {
+  const router = useRouter()
+
+  if (router.isFallback) {
+    return <div>Carregando...</div>
+  }
+
   return (
     <Header />
   )
 }
 
-// export const getStaticPaths = async () => {
-//   const prismic = getPrismicClient();
-//   const posts = await prismic.query(TODO);
+export const getStaticPaths: GetStaticPaths = async () => {
+  const prismic = getPrismicClient();
+  const posts = await prismic.query([
+    Prismic.predicates.at('document.type', 'posts')
+  ]);
 
-//   // TODO
-// };
+  const paths = posts.results.map(post=> {
+    return {
+      params: {
+        slug: post.uid
+      }
+    }
+  })
+
+  return {
+    paths,
+    fallback: true
+  }
+};
 
 export const getStaticProps: GetStaticProps = async context => {
   const prismic = getPrismicClient();
-  const response = await prismic.getByUID('posts', 'slug', {});
+  const { slug } = context.params
+  const response = await prismic.getByUID('posts', String(slug), {});
 
   const post = {
     first_publication_date: format(new Date(), ('dd-MM-yyyy'), 
@@ -58,12 +82,12 @@ export const getStaticProps: GetStaticProps = async context => {
         url: response.data.banner.url
       },
       author: response.data.author,
-      content: {
-        heading: response.data.content.heading,
-        body: {
-          text: response.data.content.body.text
+      content: response.data.content.map(content => {
+        return {
+          heading: content.heading,
+          body: [...content.body]
         }
-      }
+      })
     }
   }
 
